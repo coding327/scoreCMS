66b8385054911d0c3cfa490d76f18c401808cd10
black jack <1545721718@qq.com>
2023-03-13 10:55:09 +0800

    feat: 添加注释

--- src/views/home/ChartMap.vue
+++ src/views/home/ChartMap.vue
@@ -1,35 +1,33 @@
-
-
 <script lang="ts" setup>
-import * as echarts from 'echarts'
-import { ref , onMounted} from 'vue';
+import * as echarts from "echarts";
+import { ref, onMounted } from "vue";
 
 const props = defineProps({
-    option:{
-        type:Object,
-        default:{}
-    }
-})
-const chartRef = ref<any>()
-const ChartMapInit = ()=>{
-    // 基于准备好的dom，初始化echarts实例
+  option: {
+    type: Object,
+    default: {},
+  },
+});
+const chartRef = ref<any>();
+const ChartMapInit = () => {
+  // echarts基本使用
+  // 基于准备好的dom，初始化echarts实例
   var myChart = echarts.init(chartRef.value);
   // 指定图表的配置项和数据
-  
-    // 使用刚指定的配置项和数据显示图表。
+
+  // 使用刚指定的配置项和数据显示图表。
   myChart.setOption(props.option);
-}
+};
 
 onMounted(() => {
-    ChartMapInit()
+  ChartMapInit();
 });
 
 defineExpose({
-    ChartMapInit
-})
+  ChartMapInit,
+});
 </script>
 
-
 <template>
-    <div class="aitem" ref="chartRef"></div>
+  <div class="aitem" ref="chartRef"></div>
 </template>
